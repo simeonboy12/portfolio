96198c5465e17af3c4d0c2a8ac0c3eed78030ea0
simeonboy12 <adekoyasimeon3@gmail.com>
2026-08-07 12:31:32 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,17 @@
-function sayHello() {
-    alert("Welcome to Simeon Web Site");
-}
+const darkButton = document.getElementById("darkMode");
+
+darkButton.addEventListener("click", function () {
+
+    document.body.classList.toggle("dark");
+
+    if (document.body.classList.contains("dark")) {
+
+        darkButton.textContent = "☀️ Light Mode";
+
+    } else {
+
+        darkButton.textContent = "🌙 Dark Mode";
+
+    }
+
+});
